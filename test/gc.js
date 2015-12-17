54d8bf0753f649d34efb1a53a7803fb4c5ff6578
txiong <tomxiongzh@gmail.com>
2015-12-17 10:39:09 +0800

    1. set build node.js version to v0.10 due to this c++ source code just compatible with it 2. Add random number use object to allocation memory in gc.js test case

--- test/gc.js
+++ test/gc.js
@@ -1,24 +1,27 @@
 var v8tools = require('../build/Release/v8tools.node');
+var debug = require('debug')('node-v8tools');
 describe('v8tools', function() {
    this.timeout(300000);
    describe('#afterGC()', function () {
      it('should get gc metric  without error', function(done) {
-	var isDone = false;
-	v8tools.afterGC(function(gcType, gcFlags, usedHeapSize) {
-	  console.log('GC Type', gcType);
-	  //console.log('GC Flags', gcFlags);
-	  console.log('Used Heap Size', usedHeapSize);
-	  if (!isDone) {
-		done();
-		isDone = true;		
-	   }	
-	});
-      });
+		var isDone = false;
+		v8tools.afterGC(function(gcType, gcFlags, usedHeapSize) {
+	  		console.log('GC Type', gcType);
+	  		//console.log('GC Flags', gcFlags);
+	  		console.log('Used Heap Size', usedHeapSize);
+	  		if (!isDone) {
+				done();
+				isDone = true;
+	   		}
+		});
+	 });
    });
 });	
 
 setInterval(function() {
-  for(var i = 0; i < 1000; i++) {
+  counts = 	Math.random() * 200000;
+	console.log('The random is %s', counts);
+  for(var i = 0; i < counts; i++) {
     new Object();
   }
 }, 2000);
